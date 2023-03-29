8fa07634733ce3d767e3e0064307af9b23085323
Bagus Oktaviadi <bagusoktaviadi1@gmail.com>
2023-03-29 15:54:41 +0700

    Custom Scope

--- src/main/java/com/blanc08/belajarspringdasar/ScopeConfiguration.java
+++ src/main/java/com/blanc08/belajarspringdasar/ScopeConfiguration.java
@@ -1,7 +1,10 @@
 package com.blanc08.belajarspringdasar;
 
+import com.blanc08.belajarspringdasar.data.Bar;
 import com.blanc08.belajarspringdasar.data.Foo;
+import com.blanc08.belajarspringdasar.scope.DoubletonScope;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -15,5 +18,19 @@ public class ScopeConfiguration {
     public Foo foo(){
         log.info("Create new Foo");
         return new Foo();
+    }
+
+    @Bean
+    public CustomScopeConfigurer customScopeConfigurer(){
+        CustomScopeConfigurer configurer = new CustomScopeConfigurer();
+        configurer.addScope("doubleton", new DoubletonScope());
+        return configurer;
+    }
+
+    @Bean
+    @Scope("doubleton")
+    public Bar bar(){
+        log.info("Create new Bar");
+        return new Bar();
     }
 }
