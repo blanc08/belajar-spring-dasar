4a81868e3911d255abe5715dd6b9ba829bc2463e
Bagus Oktaviadi <bagusoktaviadi1@gmail.com>
2023-03-31 20:57:32 +0700

    Spring Boot Application | Command Line Runner

---
+++ src/main/java/com/blanc08/belajarspringdasar/commandapp/CommandApplication.java
@@ -0,0 +1,16 @@
+package com.blanc08.belajarspringdasar.commandapp;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommandApplication.class, args);
+    }
+
+}
+
+
